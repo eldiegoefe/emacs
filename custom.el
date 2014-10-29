e2208f6a4e5897f125052d2e9cc2be6f7954f0cd
eldiegoefe <eldiegoefe@gmail.com>
2014-10-29 20:29:27 -0300

    varios cambios intermedios con use-package. hasta aca funciona!

--- custom.el
+++ custom.el
@@ -17,7 +17,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("b21bf64c01dc3a34bc56fff9310d2382aa47ba6bc3e0f4a7f5af857cd03a7ef7" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" default)))
+ '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "b21bf64c01dc3a34bc56fff9310d2382aa47ba6bc3e0f4a7f5af857cd03a7ef7" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" default)))
  '(delete-selection-mode t)
  '(ergoemacs-smart-paste (quote browse-kill-ring))
  '(fci-rule-color "#383838")
@@ -40,6 +40,46 @@
  )
 
 
+;;; GENERALIDADES
+
+
+;; ------------------------------------------------------------
+;; emacs reconoce las oraciones cuando encuentra punto y dos
+;; espacios. para que las reconozca con un solo espacio:
+
+(setq sentence-end-double-space nil)
+
+
+;; -------------------------------------------------------------------
+;; configuracion de org-mode
+
+(load "~/.emacs.d/personal/config-org-mode-diego.el")
+;; (load "~/.emacs.d/personal/config-org-mode.el")
+
+
+(defmacro hook-into-modes (func modes)
+  `(dolist (mode-hook ,modes)
+     (add-hook mode-hook ,func)))
+
+;; ------------------------------------------------------------
+;; para evitar que se molesten los modos de company y yasnippet
+;; "\t" se refiere a TAB
+
+(define-key company-active-map "\t" 'company-yasnippet-or-completion)
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (if (yas/expansion-at-point)
+      (progn (company-abort)
+             (yas/expand))
+    (company-complete-common)))
+
+(defun yas/expansion-at-point ()
+  "Tested with v0.6.1. Extracted from `yas/expand-1'"
+  (first (yas/current-key)))
+
+
+
 (require 'use-package)
 
 ;; -------------------------------------------------------------------
@@ -47,6 +87,10 @@
 
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+
+
+;; USE-PACKAGE
 
 
 (use-package ergoemacs-mode
@@ -69,62 +113,9 @@
   (progn
     (setq ergoemacs-theme nil)
     (setq ergoemacs-keyboard-layout "er")
-    (ergoemacs-mode 1)
-    ))
+    (ergoemacs-mode 1)))
 
 
-
-;; -------------------------------------------------------------------
-;; ergoemacs de vuelta!
-
-;; (package-initialize)
-
-;; (setq ergoemacs-theme nil)
-;; (setq ergoemacs-keyboard-layout "er")
-;; (require 'ergoemacs-mode)
-;; (ergoemacs-mode 1)
-
-
-;; ------------------------------------------------------------
-;; ------------------------------------------------------------
-;; inicio de las configuraciones extraidas del video 8 de rt2011
-;; ------------------------------------------------------------
-
-
-;; ------------------------------------------------------------
-;; emacs server
-;; do not close the file that was being edited when C-x # is typed
-(setq server-kill-new-buffers nil)
-
-;; ------------------------------------------------------------
-;; start the emacs server for emacsclient
-;; (server-start)
-
-;; ------------------------------------------------------------
-;; setup custom shortcuts
-(global-set-key "\C-x\C-g" 'goto-line)
-
-(global-set-key (kbd "<C-f5>") 'compile)
-(global-set-key (kbd "<C-f6>") 'next-error)
-
-;; -------------------------------------------------------------------
-;; configuracion de org-mode
-
-(load "~/.emacs.d/personal/config-org-mode-diego.el")
-;; (load "~/.emacs.d/personal/config-org-mode.el")
-
-
-;; ------------------------------------------------------------
-;; fin de las configuraciones extraidas del video 8 de rt2011
-;; ------------------------------------------------------------
-;; ------------------------------------------------------------
-
-(defmacro hook-into-modes (func modes)
-  `(dolist (mode-hook ,modes)
-     (add-hook mode-hook ,func)))
-
-;; ------------------------------------------------------------
-;; yasnippets
 
 (use-package yasnippet
   :ensure yasnippet
@@ -136,55 +127,82 @@
                      rst-mode-hook)))
 
 
+
+(use-package workgroups2
+  ;; gestor de sesiones (guarda buffers y ventanas)
+  :ensure workgroups2
+  :init
+  (workgroups-mode 1))
+
+
+
+(use-package smart-mode-line
+  :ensure smart-mode-line
+  :config
+  (progn
+    (sml/setup)
+    ;;(sml/apply-theme 'dark)
+    ;;;;(sml/apply-theme 'light)
+    ;;(sml/apply-theme 'respectful)
+    ;;(sml/apply-theme 'automatic)
+    (sml/apply-theme 'powerline)))
+
+
+
+(use-package aggressive-indent
+  ;; nuevo modo de indentación (creado por artur malabarba), reemplaza
+  ;; al modo electric-indent-mode
+  ensure: aggressive-indent
+  init:
+  (global-aggressive-indent-mode))
+
+
+
+
+(use-package popup
+  :ensure popup
+  :commands popup-tip)  ; el comando popup-tip lo saqué por prueba y
+                        ; error. si ponía popup solamente, la función
+                        ; describe-thing-in-popup no funcionaba
+
+
+(use-package help-fns+
+  ;; help-fns+ mejora las ayudas. además, se agrega una función para
+  ;; que la ayuda sobre la función debajo del punto se abra en una
+  ;; ventanita (sin ocupar un buffer aparte). Esto último me lo
+  ;; recomendaron en mi pregunta en Emacs.SE: how to get the function
+  ;; help without typing.
+  :ensure help-fns+
+  :init
+  (defun describe-thing-in-popup ()
+    (interactive)
+    (let* ((thing (symbol-at-point))
+           (help-xref-following t)
+           (description (save-window-excursion
+                          (with-temp-buffer
+                            (help-mode)
+                            (help-xref-interned thing)
+                            (buffer-string)))))
+      (popup-tip description
+                 :point (point)
+                 :around t
+                 :height 20
+                 :scroll-bar t
+                 :margin t))))
+
+
+
+
+
+
+
+
+;; KEYBINDINGS
+
+
 ;; ------------------------------------------------------------
-;; para evitar que se molesten los modos de company y yasnippet
-;; "\t" se refiere a TAB
-
-(define-key company-active-map "\t" 'company-yasnippet-or-completion)
-
-(defun company-yasnippet-or-completion ()
-  (interactive)
-  (if (yas/expansion-at-point)
-      (progn (company-abort)
-             (yas/expand))
-    (company-complete-common)))
-
-(defun yas/expansion-at-point ()
-  "Tested with v0.6.1. Extracted from `yas/expand-1'"
-  (first (yas/current-key)))
-
-
-
-;; ------------------------------------------------------------
-;; slime
-
-;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-;; (setq inferior-lisp-program "sbcl")
-
-
-;; ------------------------------------------------------------
-;; para que M-S-down y M-S-up funcionen en org-mode
-
-;; (define-key prelude-mode-map [(meta shift up)] nil)
-;; (define-key prelude-mode-map [(meta shift down)] nil)
-
-
-;; ------------------------------------------------------------
-;; para que M-o cambie de buffer en vez de ser otro S-Enter
-
-(define-key prelude-mode-map (kbd "M-o") nil)
-
-
-;; ------------------------------------------------------------
-;; mis definiciones de teclas
-
-;; (global-set-key (kbd "<f2>") 'kill-ring-save)
-;; (global-set-key (kbd "<f3>") 'yank)
-
-;; en /.emacs.d/core/prelude-global-keybindings.el está aun la
-;; definición con <f12> pero la cambio acá para probar las cosas del
-;; tutorial de org-mode. Queda entonces redundante, pero anda...
+;; cambiar M-x a la version power de helm
+(global-set-key (kbd "M-a") 'helm-M-x)
 
 (global-set-key (kbd "<M-f12>") 'menu-bar-mode)
 
@@ -198,94 +216,13 @@
 
 
 ;; ------------------------------------------------------------
-;; setear ipython -sin el notebook-
-;; ("How to open IPython interpreter in emacs?" en stackexchange)
+;; para que M-o cambie de buffer en vez de ser otro S-Enter
 
-;; (when (executable-find "ipython")
-;;   (setq
-;;    python-shell-interpreter "ipython"
-;;    python-shell-interpreter-args ""
-;;    python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;    python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;    python-shell-completion-setup-code
-;;    "from IPython.core.completerlib import module_completion"
-;;    python-shell-completion-module-string-code
-;;    "';'.join(module_completion('''%s'''))\n"
-;;    python-shell-completion-string-code
-;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
-
-
-;; ------------------------------------------------------------
-;; gestor de sesiones (guarda buffers y ventanas)
-
-(require 'workgroups2)
-(workgroups-mode 1)
-
-
-;; ------------------------------------------------------------
-;; cambiar M-x a la version power de helm
-(global-set-key (kbd "M-a") 'helm-M-x)
-
-;; ------------------------------------------------------------
-;; emacs reconoce las oraciones cuando encuentra punto y dos
-;; espacios. para que las reconozca con un solo espacio:
-
-(setq sentence-end-double-space nil)
-
-
-;; ------------------------------------------------------------
-;; smart mode line
-
-(sml/setup)
-;;(sml/apply-theme 'dark)
-;;(sml/apply-theme 'light)
-;;(sml/apply-theme 'respectful)
-;;(sml/apply-theme 'automatic)
-(sml/apply-theme 'powerline)
-
-
-;; ------------------------------------------------------------
-;; guide key abre un buffer con las opciones de keybindings
-;; correspondientes a cada prefix-keys que se vayan apretando
-
-;; (require 'guide-key)
-;; (setq guide-key/guide-key-sequence '("C-x" "C-c"))
-;; (setq guide-key/recursive-key-sequence-flag t)
-;; (guide-key-mode 1)
-
-
-;; -------------------------------------------------------------------
-;; nuevo modo de indentación de artur malabarba, reemplaza
-;; electric-indent-mode
-
-(global-aggressive-indent-mode)
-
-
-;; ------------------------------------------------------------
-;; mejora en las ayudas. de mi pregunta en Emacs.SE:
-;; http://emacs.stackexchange.com/questions/2777/how-to-get-the-function-help-without-typing
-
-(require 'popup)
-
-(defun describe-thing-in-popup ()
-  (interactive)
-  (let* ((thing (symbol-at-point))
-         (help-xref-following t)
-         (description (save-window-excursion
-                        (with-temp-buffer
-                          (help-mode)
-                          (help-xref-interned thing)
-                          (buffer-string)))))
-    (popup-tip description
-               :point (point)
-               :around t
-               :height 20
-               :scroll-bar t
-               :margin t)))
+(define-key prelude-mode-map (kbd "M-o") nil)
 
 (global-set-key (kbd "s-l") 'describe-thing-in-popup)
 
-(require 'help-fns+)
+
 
 ;; ------------------------------------------------------------
 ;; fuera de uso
@@ -312,3 +249,72 @@
 ;; customize variable.
 
 ;; (setq yas-snippet-dirs '("~/emacs.d/personal/misSnippets"))
+
+
+
+;; ------------------------------------------------------------
+;; emacs server
+;; do not close the file that was being edited when C-x # is typed
+;; (setq server-kill-new-buffers nil)
+
+;; ------------------------------------------------------------
+;; start the emacs server for emacsclient
+;; (server-start)
+
+;; ------------------------------------------------------------
+;; setup custom shortcuts
+;; (global-set-key "\C-x\C-g" 'goto-line)
+
+;; (global-set-key (kbd "<C-f5>") 'compile)
+;; (global-set-key (kbd "<C-f6>") 'next-error)
+
+;; ------------------------------------------------------------
+;; guide key abre un buffer con las opciones de keybindings
+;; correspondientes a cada prefix-keys que se vayan apretando
+
+;; (require 'guide-key)
+;; (setq guide-key/guide-key-sequence '("C-x" "C-c"))
+;; (setq guide-key/recursive-key-sequence-flag t)
+;; (guide-key-mode 1)
+
+
+;; ------------------------------------------------------------
+;; slime
+
+;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+;; (setq inferior-lisp-program "sbcl")
+
+
+
+;; ------------------------------------------------------------
+;; mis definiciones de teclas
+
+;; (global-set-key (kbd "<f2>") 'kill-ring-save)
+;; (global-set-key (kbd "<f3>") 'yank)
+
+
+
+;; ------------------------------------------------------------
+;; setear ipython -sin el notebook-
+;; ("How to open IPython interpreter in emacs?" en stackexchange)
+
+;; (when (executable-find "ipython")
+;;   (setq
+;;    python-shell-interpreter "ipython"
+;;    python-shell-interpreter-args ""
+;;    python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;    python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;    python-shell-completion-setup-code
+;;    "from IPython.core.completerlib import module_completion"
+;;    python-shell-completion-module-string-code
+;;    "';'.join(module_completion('''%s'''))\n"
+;;    python-shell-completion-string-code
+;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+
+;; ------------------------------------------------------------
+;; para que M-S-down y M-S-up funcionen en org-mode
+
+;; (define-key prelude-mode-map [(meta shift up)] nil)
+;; (define-key prelude-mode-map [(meta shift down)] nil)
